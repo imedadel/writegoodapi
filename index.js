98adf59296b5c8047e5fd3f3d32b551827e22af2
Imed Adel <aadelimed@gmail.com>
2019-05-11 09:30:08 +0100

    Remove koa-route

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Koa = require("koa");
 const koaBody = require("koa-body");
-const _ = require("koa-route");
 const writeGood = require("write-good");
 
 const app = (module.exports = new Koa());
@@ -15,34 +14,34 @@ app.use(
 // co-body accepts application/json
 // and application/x-www-form-urlencoded
 
-// app.use(async function(ctx) {
-//   const body = ctx.request.body;
-//   if (!body.text) ctx.throw(400, '.text required');
-//   if(!body.options) ctx.body = writeGood(body.text);
-//   else {
-//     if(typeof(body.options) !== "object") ctx.throw(400, '.options should be an object');
-//     ctx.body = writeGood(body.text,body.options);
-//   }
-// });
+app.use(async function(ctx) {
+  const body = ctx.request.body;
+  if (!body.text) ctx.throw(400, '.text required');
+  if(!body.options) ctx.body = writeGood(body.text);
+  else {
+    if(typeof(body.options) !== "object") ctx.throw(400, '.options should be an object');
+    ctx.body = writeGood(body.text,body.options);
+  }
+});
 
-app.use(
-  _.get("/", async ctx => {
-    ctx.body =
-      "Send a POST request to /api containing `text` (string) and, optionally, `options` (object).";
-  })
-);
+// app.use(
+//   _.get("/", async ctx => {
+//     ctx.body =
+//       "Send a POST request to /api containing `text` (string) and, optionally, `options` (object).";
+//   })
+// );
 
-app.use(
-  _.get("/api", async ctx => {
-    const body = ctx.request.body;
-    if (!body.text) ctx.throw(400, ".text required");
-    if (!body.options) ctx.body = writeGood(body.text);
-    else {
-      if (typeof body.options !== "object")
-        ctx.throw(400, ".options should be an object");
-      ctx.body = writeGood(body.text, body.options);
-    }
-  })
-);
+// app.use(
+//   _.get("/api", async ctx => {
+//     const body = ctx.request.body;
+//     if (!body.text) ctx.throw(400, ".text required");
+//     if (!body.options) ctx.body = writeGood(body.text);
+//     else {
+//       if (typeof body.options !== "object")
+//         ctx.throw(400, ".options should be an object");
+//       ctx.body = writeGood(body.text, body.options);
+//     }
+//   })
+// );
 
 if (!module.parent) app.listen(port);
